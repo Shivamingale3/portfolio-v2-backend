2202f4802461fa5d474ace1ef26af34b63a28b47
Shivamingale3 <shivamingale3@gmail.com>
2025-02-02 23:02:49 +0530

    [FIX]: Refactor

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -12,6 +12,7 @@ class ProfileRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/get`, profileController.getProfile);
+    this.router.post(`${this.path}/update`, profileController.updateProfile);
   }
 }
 
